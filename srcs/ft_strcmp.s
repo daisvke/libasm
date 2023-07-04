666764d3298ec323b9e3ccd6b7831010b7535124
d <dtanigaw@student.42.fr>
2023-07-04 04:17:43 +0200

    Update ft_strcmp.s

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -12,7 +12,7 @@ ft_strcmp:
 	movzx	rax, byte [rdi] ; Load 1st byte from rdi, zero extend into rax
 	movzx	rbx, byte [rsi] ; Load 1st byte from rsi, zero extend into rbx
 	sub		rax, rbx		; Compute the difference
-	jne		exit			; If result is 0, exit loop
+	jne		exit			; If not equal, exit loop
 
 	cmp		byte [rdi], 0	; Check if we arrived at the end of rdi
 	je		exit			; If true, exit from loop
